578ad6d5ff08b06eb1fb956aa17140c397cad5b6
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-02-15 17:13:37 -0300

    profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" placeholder="Seu endereço de e-mail" />
 
@@ -26,6 +31,8 @@ export default function Profile() {
 
         <button type="submit">Atualizar perfil</button>
       </Form>
+
+      <button type="button">Sair do GoBarber</button>
     </Container>
   );
 }
