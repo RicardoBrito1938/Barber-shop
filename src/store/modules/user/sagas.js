53ade6407ff34ed43ed7116b88a963a1f8c84efa
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-02-19 17:44:04 -0300

    update Profile fix1

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,29 +1,25 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
-import {toast} from 'react-toastify'
-import api from '~/services/api'
+import { toast } from 'react-toastify';
+import api from '~/services/api';
 
-import {updateProfileSuccess, updateProfileFailure} from './actions'
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
-export function updateProfile({ payload }) {
+export function* updateProfile({ payload }) {
   try {
     const { name, email, ...rest } = payload.data;
 
     const profile = Object.assign(
-      {
-        name,
-        email,
-      },
+      { name, email },
       rest.oldPassword ? rest : {}
     );
-  
-    const response = yield call(api.put, 'users', profile)
-  
-    toast.success('Perfil atualizado com sucesso!')
-    
-    yield put(updateProfileSuccess(response.data))
-  } catch(err) {
-    toast.success('Erro ao atualizar perfil, confira seus dados')
-      yield put(updateProfileFailure())
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seus dados');
+    yield put(updateProfileFailure());
   }
 }
 
